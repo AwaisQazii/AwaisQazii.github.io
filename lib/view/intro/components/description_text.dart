cd45ddc7e1fe609b8090e2bd1202748f50898c47
AwaisQazii <awaisqazi639@gmail.com>
2024-02-14 18:00:11 +0500

    BIO updated

--- lib/view/intro/components/description_text.dart
+++ lib/view/intro/components/description_text.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/res/constants.dart';
 import 'package:flutter_portfolio/view%20model/responsive.dart';
-import 'package:flutter_portfolio/view/intro/components/subtitle_text.dart';
-import 'package:get/get.dart';
 
 class AnimatedDescriptionText extends StatelessWidget {
   const AnimatedDescriptionText(
@@ -14,54 +14,57 @@ class AnimatedDescriptionText extends StatelessWidget {
       tween: Tween(begin: start, end: end),
       duration: const Duration(milliseconds: 200),
       builder: (context, value, child) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Experienced Mobile Development Specialist and Software Engineer${Responsive.isMobile(context) ? '\n' : ' '}adept in Flutter\nwith a proven track record of delivering innovative mobile solutions.\nOwner of MobilePulse, dedicated to crafting user-centric \napplications that drive engagement and enhance user experiences.',
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                  color: Colors.grey, wordSpacing: 2, fontSize: value),
+        return RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                  style: const TextStyle(color: primaryColor),
+                  text:
+                      'Experienced Mobile Development Specialist and ${Responsive.isMobile(context) ? '\n' : ''}Software Engineer adept in Flutter with a${Responsive.isDesktop(context) || Responsive.isMobile(context) ? '\n' : ''}proven track record of delivering innovative ${Responsive.isLargeMobile(context) || Responsive.isMobile(context) ? '\n' : ''}mobile solutions.\nOwner of '),
+              TextSpan(
+                  style: const TextStyle(
+                      color: Colors.red, fontWeight: FontWeight.w800),
+                  text: 'MobilePulse',
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => showMobilePulseDialog(context)),
+              TextSpan(
+                  style: const TextStyle(color: primaryColor),
+                  text:
+                      ', dedicated to crafting${Responsive.isLargeMobile(context) || Responsive.isMobile(context) ? '\n' : ''}user-centric applications${Responsive.isDesktop(context) || Responsive.isMobile(context) ? '\n' : ''}that drive engagement and enhanceuser experiences.')
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<dynamic> showMobilePulseDialog(BuildContext context) {
+    return showAdaptiveDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          content: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/image.png',
+                  height: 200,
+                  width: 300,
+                ),
+                const Text(
+                  'Welcome to Mobile Pulse, your premier destination for cutting-edge mobile app solutions. As a dynamic startup, we specialize in crafting bespoke mobile applications tailored to your unique needs. With a passionate team of developers and designers, we\'re committed to delivering innovative solutions that propel your business forward in the digital landscape. (We\'re coming soon)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-                onTap: () {
-                  showAdaptiveDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        backgroundColor: Colors.white,
-                        content: Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                'assets/images/image.png',
-                                height: 200,
-                                width: 300,
-                              ),
-                              const Text(
-                                'Welcome to Mobile Pulse, your premier destination for cutting-edge mobile app solutions. As a dynamic startup, we specialize in crafting bespoke mobile applications tailored to your unique needs. With a passionate team of developers and designers, we\'re committed to delivering innovative solutions that propel your business forward in the digital landscape. (We\'re coming soon)',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                                textAlign: TextAlign.center,
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-                child: AnimatedSubtitleText(
-                    start: start, end: end, text: 'MobilePulse')),
-          ],
+          ),
         );
       },
     );
