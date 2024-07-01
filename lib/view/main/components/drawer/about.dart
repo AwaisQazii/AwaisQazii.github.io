ddccf6b189663d9196a1f0c2b144c570c9fa3a39
AwaisQazii <awaisqazi639@gmail.com>
2024-07-01 14:25:19 +0500

    name added

--- lib/view/main/components/drawer/about.dart
+++ lib/view/main/components/drawer/about.dart
@@ -9,7 +9,7 @@ class About extends StatelessWidget {
     return AspectRatio(
       aspectRatio: 1.23,
       child: Container(
-        color: bgColor,
+        color: Theme.of(context).scaffoldBackgroundColor,
         child: Column(
           children: [
             const Spacer(
@@ -19,15 +19,19 @@ class About extends StatelessWidget {
             const Spacer(),
             Text(
               'Awais Qazi',
-              style: Theme.of(context).textTheme.titleSmall,
+              style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: Theme.of(context).textTheme.bodyMedium?.color),
             ),
             const SizedBox(
               height: defaultPadding / 4,
             ),
-            const Text(
+            Text(
               'Flutter Developer',
               textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.w200, height: 1.5),
+              style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  height: 1.5,
+                  color: Theme.of(context).textTheme.bodyMedium?.color),
             ),
             const Spacer(
               flex: 2,
